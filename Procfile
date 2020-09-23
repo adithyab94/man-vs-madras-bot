56642c738478cc0a444eeda4e453264b2fd88559
adithyab94 <adithya.b94@gmail.com>
2020-09-24 00:39:22 +0200

    add nlp changes

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 clock: python tweet_bot.py
+clock: python tweet_bot2.py
---
+++ tweet_bot2.py
@@ -0,0 +1,33 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
+from random import choice
+import tweepy
+import os
+from os import environ
+
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+sched = BlockingScheduler()
+                    
+@sched.scheduled_job('interval', hours=6)
+def timed_job():
+    s=open("samples.txt","r",encoding="utf-8")
+    m=s.readlines()
+    l=[]
+    for i in range(0,len(m)-1):
+        x=m[i]
+        z=len(x)
+        a=x[:z-1]
+        l.append(a)
+    l.append(m[i+1])
+    o=choice(l)
+    api.update_status(o)
+    s.close()
+
+sched.start()
